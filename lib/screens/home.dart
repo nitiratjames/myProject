b054759828c89bb736caefed3095d86bb4d90865
Asus <james_nitirat@hotmail.com>
2020-11-12 18:09:09 +0700

    aa

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
+import 'package:checkpoint/screens/register.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,11 +12,47 @@ class _HomeState extends State<Home> {
     return Text(
       'ฮาว่าละ !!!',
       style: TextStyle(
-        fontSize: 40.0,
-        color: Colors.black,
+        fontSize: 70.0,
         fontWeight: FontWeight.bold,
+        color: Colors.black87,
         fontFamily: 'Kanit',
       ),
+    );
+  }
+
+  Widget showAppTitle() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        SizedBox(width: 0.0, height: 100.0),
+        Text(
+          "SAFE ",
+          style: TextStyle(
+            fontSize: 40.0,
+            fontFamily: 'Architects',
+            // fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(width: 0.0, height: 100.0),
+        RotateAnimatedTextKit(
+            duration: Duration(
+              milliseconds: 2000,
+            ),
+            onTap: () {
+              print("Tap Event");
+            },
+            text: [
+              "FIRST",
+              "LIFE",
+              "FREEDOM",
+            ],
+            textStyle: TextStyle(
+              fontSize: 40.0,
+              fontFamily: "Architects",
+              // decoration: TextDecoration.underline,
+            ),
+            textAlign: TextAlign.start),
+      ],
     );
   }
 
@@ -27,30 +65,65 @@ class _HomeState extends State<Home> {
   }
 
   Widget singInButton() {
-    return RaisedButton(
-      color: Colors.blue,
-      child: Text(
-        'Sign In',
-        style: TextStyle(
+    return ButtonTheme(
+      minWidth: 150.0,
+      height: 50.0,
+      child: RaisedButton.icon(
+        onPressed: () {
+          print('Button Clicked.');
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        label: Text(
+          'ลงชื่อเข้าใช้',
+          style: TextStyle(
+            fontSize: 25.0,
+            color: Colors.white,
+            fontFamily: 'Kanit',
+          ),
+        ),
+        icon: Icon(
+          Icons.keyboard,
           color: Colors.white,
         ),
+        textColor: Colors.white,
+        splashColor: Colors.red,
+        color: Colors.blue,
       ),
-      onPressed: () {},
     );
   }
 
   Widget singUpButton() {
-    return RaisedButton(
-      color: Colors.pink.shade300,
-      child: Text(
-        'Sign Up',
-        style: TextStyle(
+    return ButtonTheme(
+      minWidth: 150.0,
+      height: 50.0,
+      child: RaisedButton.icon(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Register()),
+          );
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        label: Text(
+          'ลงทะเบียน',
+          style: TextStyle(
+            fontSize: 25.0,
+            color: Colors.white,
+            fontFamily: 'Kanit',
+          ),
+        ),
+        icon: Icon(
+          Icons.note_add,
           color: Colors.white,
         ),
+        textColor: Colors.white,
+        splashColor: Colors.red,
+        color: Colors.green,
       ),
-      onPressed: () {
-        print('gggggggggg');
-      },
     );
   }
 
@@ -74,11 +147,12 @@ class _HomeState extends State<Home> {
         child: Container(
           decoration: BoxDecoration(
             gradient: RadialGradient(
-              colors: [
+              center: const Alignment(-0.5, -0.7),
+              radius: 0.8,
+              colors: <Color>[
                 Colors.white,
                 Colors.pink.shade200,
               ],
-              radius: 1.1,
             ),
           ),
           child: Center(
@@ -86,6 +160,10 @@ class _HomeState extends State<Home> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 showAppName(),
+                SizedBox(
+                  height: 100.0,
+                ),
+                showAppTitle(),
                 SizedBox(
                   height: 50.0,
                 ),
